a97f78181ac8a5b34d820587d90a8d3f780da002
kevinrs5855 <kevinrs@gmail.com>
2016-04-27 16:53:32 -0400

    rebased

--- notebooks/Users/Kevin.Schmidt@icims.com/GitHubIntegrationTestNotebook.scala
+++ notebooks/Users/Kevin.Schmidt@icims.com/GitHubIntegrationTestNotebook.scala
@@ -1,18 +1,18 @@
-// Databricks notebook source exported at Wed, 27 Apr 2016 20:52:00 UTC
-// MAGIC %md my test notebook with an initial comment 4:22PM
+// Databricks notebook source exported at Wed, 27 Apr 2016 20:53:12 UTC
+// MAGIC %md my test notebook with an initial comment 4:22PM%md my test notebook with an initial comment 4:22PM
 
 // COMMAND ----------
 
-// MAGIC %sql SELECT 1
+// MAGIC %sql SELECT 1%sql SELECT 1
 
 // COMMAND ----------
 
-val test = "my test"
+val test = "my test"val test = "my test"
 
 // COMMAND ----------
 
-// MAGIC %r rnorm(24, mean=pi, sd=0.1)
+// MAGIC %r rnorm(24, mean=pi, sd=0.1)%r rnorm(24, mean=pi, sd=0.1)
 
 // COMMAND ----------
 
-val masteraddition = "git master add"
+val newstuff = "gitness"val masteraddition = "git master add"
