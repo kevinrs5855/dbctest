23883dabf09426df65c1c693e7915e302006d018
kevinrs5855 <kevinrs@gmail.com>
2016-04-27 16:46:51 -0400

    Committing changes

--- notebooks/Users/Kevin.Schmidt@icims.com/GitHubIntegrationTestNotebook.scala
+++ notebooks/Users/Kevin.Schmidt@icims.com/GitHubIntegrationTestNotebook.scala
@@ -1,1 +1,17 @@
-// Databricks notebook source exported at Wed, 27 Apr 2016 19:52:14 UTC
+// Databricks notebook source exported at Wed, 27 Apr 2016 20:46:30 UTC
+// MAGIC %md my test notebook with an initial comment 4:22PM
+
+// COMMAND ----------
+
+// MAGIC %sql SELECT 1
+
+// COMMAND ----------
+
+val test = "my test"
+
+// COMMAND ----------
+
+// MAGIC %r rnorm(24, mean=pi, sd=0.1)
+
+// COMMAND ----------
+
